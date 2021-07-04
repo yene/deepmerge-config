acfdeeeee4dd527dbaa9241c1d69766be48df1c4
Yannick Weiss <hi@yannick.dev>
2021-07-04 17:23:45 +0200

    Added stackoverflow snippets for comparison to the npm package

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 let test = require('ava')
-let merge = require('deepmerge')
 let fs = require('fs')
+
+
+/*
+ * using the deepmege package
+*/
+
+let merge = require('deepmerge')
 
 function deepMerge(...sources) {
   return merge.all(sources)
@@ -28,5 +34,69 @@ test('test_arrayreplace', t => {
   let patch3 = JSON.parse(fs.readFileSync('test_arrayreplace/patch3.json'))
   let result = JSON.parse(fs.readFileSync('test_arrayreplace/result.json'))
   let r = deepMergeReplace(original, patch1, patch2, patch3)
+  t.deepEqual(r, result)
+});
+
+/*
+ * from https://stackoverflow.com/a/49798508/279890
+*/
+
+function so_deepMerge(...sources) {
+  let acc = {}
+  for (const source of sources) {
+    if (source instanceof Array) {
+      if (!(acc instanceof Array)) {
+        acc = []
+      }
+      acc = [...acc, ...source]
+    } else if (source instanceof Object) {
+      for (let [key, value] of Object.entries(source)) {
+        if (value instanceof Object && key in acc) {
+          value = deepMerge(acc[key], value)
+        }
+        acc = { ...acc, [key]: value }
+      }
+    }
+  }
+  return acc
+}
+
+function so_deepMergeReplace(...sources) {
+  let acc = {}
+  for (const source of sources) {
+    if (source instanceof Array) {
+      if (!(acc instanceof Array)) {
+        acc = []
+      }
+      acc = source
+    } else if (source instanceof Object) {
+      for (let [key, value] of Object.entries(source)) {
+        if (value instanceof Object && key in acc) {
+          value = deepMergeReplace(acc[key], value)
+        }
+        acc = { ...acc, [key]: value }
+      }
+    }
+  }
+  return acc
+}
+
+test('test_so_arraymerge', t => {
+  let original = JSON.parse(fs.readFileSync('test_arraymerge/original.json'))
+  let patch1 = JSON.parse(fs.readFileSync('test_arraymerge/patch1.json'))
+  let patch2 = JSON.parse(fs.readFileSync('test_arraymerge/patch2.json'))
+  let patch3 = JSON.parse(fs.readFileSync('test_arraymerge/patch3.json'))
+  let result = JSON.parse(fs.readFileSync('test_arraymerge/result.json'))
+  let r = so_deepMerge(original, patch1, patch2, patch3)
+  t.deepEqual(r, result)
+});
+
+test('test_so_arrayreplace', t => {
+  let original = JSON.parse(fs.readFileSync('test_arrayreplace/original.json'))
+  let patch1 = JSON.parse(fs.readFileSync('test_arrayreplace/patch1.json'))
+  let patch2 = JSON.parse(fs.readFileSync('test_arrayreplace/patch2.json'))
+  let patch3 = JSON.parse(fs.readFileSync('test_arrayreplace/patch3.json'))
+  let result = JSON.parse(fs.readFileSync('test_arrayreplace/result.json'))
+  let r = so_deepMergeReplace(original, patch1, patch2, patch3)
   t.deepEqual(r, result)
 });
